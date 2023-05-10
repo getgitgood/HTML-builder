401ef1ee76bfe46936f35b9ceb9aca545a977d11
Kotec <vivalastelmah@gmail.com>
2023-05-10 15:20:33 +0300

    fix: one uppercase letter what brake the whole script in task four

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,22 +2,19 @@ const fs = require('fs');
 const { mkdir, rm, readdir, copyFile } = fs.promises;
 const path = require('path');
 
-const copyPath = path.join(__dirname, 'files-copy');
-const sourcePath = path.join(__dirname, 'files');
+let copyPath = path.join(__dirname, 'files-copy');
+let sourcePath = path.join(__dirname, 'files');
 
 const optRmdir = {recursive: true, force: true};
-const optReaddir = {WithFileTypes: true};
+const optReaddir = {withFileTypes: true};
 const optMkdir = {recursive: true};
-
-
-
 
 async function copyDir(dist, from) {
   await mkdir(dist, optMkdir);
   const files = await readdir(from, optReaddir);
   for (const file of files) {
-    const srcPath = path.join(from, file.name);
-    const destPath = path.join(dist, file.name);
+    let srcPath = path.join(from, file.name);
+    let destPath = path.join(dist, file.name);
     if (file.isDirectory()) {
       await copyDir(destPath, srcPath);
     } else {
